90dc3bdd983cf8ad06f9f78bf0eecb7c20a069ff
masterAULD <auld100@gmail.com>
2019-05-23 15:57:44 +0700

    update 4

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prj01/screens/register.dart';
+import 'package:http/http.dart' show get;
+import 'dart:convert';
 
 class Authen extends StatefulWidget {
   @override
@@ -7,6 +9,9 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  final formKey = GlobalKey<FormState>();
+  String user, password;
+
   Widget showTitle(String nText, double nFsize) {
     return Text(
       nText,
@@ -24,14 +29,30 @@ class _AuthenState extends State<Authen> {
   Widget showUser() {
     return TextFormField(
       decoration: InputDecoration(labelText: "USER: ", hintText: "Username"),
+      validator: (String value) {
+        if (value.length == 0) {
+          return "Please fill in your username";
+        }
+      },
+      onSaved: (String value) {
+        user = value;
+      },
     );
   }
 
   Widget showPassword() {
-    return TextField(
+    return TextFormField(
       decoration:
           InputDecoration(labelText: "PASSWORD: ", hintText: "Password"),
+      validator: (String value) {
+        if (value.length <= 5) {
+          return "Password should be more then 5 characters.";
+        }
+      },
       obscureText: true,
+      onSaved: (String value) {
+        password = value;
+      },
     );
   }
 
@@ -55,55 +76,77 @@ class _AuthenState extends State<Authen> {
       child: Text("Sign In"),
       color: Colors.lightGreen[100],
       onPressed: () {
-        print("You Click SignUp");
+        print("You Click SignIn");
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print("SignIn OK");
+          checkUserAndPassword();
+        }
       },
     );
+  }
+
+  void checkUserAndPassword() async {
+    String urlPHP =
+        'http://www.androidthai.in.th/tid/getUserWhereUserMaster.php?isAdd=true&User=$user';
+    var response = await get(urlPHP);
+    var resultString = json.decode(response.body);
+    print('resultString ==> $resultString');
+    if (resultString['Password'].toString() == password.toString()) {
+      //Create POP
+      print("login ok.");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomPadding: false,
-        body: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.tealAccent[100], Colors.white],
-                    begin: Alignment(0, 1))),
-            padding: EdgeInsets.only(top: 70.0),
-            alignment: Alignment(0, 0),
-            child: Column(
-              children: <Widget>[
-                showLogo(),
-                //showTitle("", 16.0),
-                Container(
-                  margin: EdgeInsets.only(
-                    top: 15.0,
-                  ),
-                  child: showTitle("User Authentication", 28.0),
+      resizeToAvoidBottomPadding: false,
+      body: Form(
+        key: formKey,
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.tealAccent[100], Colors.white],
+                  begin: Alignment(0, 1))),
+          padding: EdgeInsets.only(top: 70.0),
+          alignment: Alignment(0, 0),
+          child: Column(
+            children: <Widget>[
+              showLogo(),
+              //showTitle("", 16.0),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 15.0,
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 15, top: 0, right: 15),
-                  child: showUser(),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 15, top: 0, right: 15),
-                  child: showPassword(),
-                ),
-                Container(
-                    margin: EdgeInsets.only(left: 15, top: 10, right: 15),
-                    alignment: Alignment(0, 0),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: showSignIn(),
-                        ),
-                        Expanded(
-                          child: showSignUp(context),
-                        )
-                      ],
-                    ))
-                //showTitle("", 16.0)
-              ],
-            )));
+                child: showTitle("User Authentication", 28.0),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15, top: 0, right: 15),
+                child: showUser(),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15, top: 0, right: 15),
+                child: showPassword(),
+              ),
+              Container(
+                  margin: EdgeInsets.only(left: 15, top: 10, right: 15),
+                  alignment: Alignment(0, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: showSignIn(),
+                      ),
+                      Expanded(
+                        child: showSignUp(context),
+                      )
+                    ],
+                  ))
+              //showTitle("", 16.0)
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
